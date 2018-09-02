0be599aaeb5d39dfbeb465eee60fb73c34f94a6e
Ivan Shafran <shafran.fun@gmail.com>
2018-09-02 18:29:55 +0300

    Add mockito with final mocking error test

--- app/src/test/kotlin/org/spekframework/speksample/CalculatorSpec.kt
+++ app/src/test/kotlin/org/spekframework/speksample/CalculatorSpec.kt
@@ -1,12 +1,13 @@
 package org.spekframework.speksample
 
+import org.mockito.Mockito
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
 
 object CalculatorSpec : Spek({
     describe("A Calculator") {
-        val calculator by memoized { Calculator() }
+        val calculator = Mockito.mock(Calculator::class.java)
 
         it("should return 4") {
             assertEquals(expected = 4, actual = calculator.add(2, 2))
